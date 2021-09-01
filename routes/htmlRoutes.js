8af0d66f4e64b8fa03121a994e5eb4433b1c35f5
gmunoz94 <g.munozii@icloud.com>
2021-08-31 21:56:00 -0500

    fixed html routes and public folder structure

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const path = require('path')
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/html/index.html"))
 })
 
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/stats.html"))
+})
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/exercise.html"))
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
 });
 
 // routes
-app.use(require(routes));
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
