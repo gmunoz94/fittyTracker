13955212d4b18663c457b313c964857172a19c50
gmunoz94 <g.munozii@icloud.com>
2021-09-06 12:18:30 -0500

    fixed workout model and server routes

---
+++ models/workout.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    weight: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    }
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- routes/api/api.js
+++ routes/api/api.js
@@ -1,15 +1,38 @@
 const router = require("express").Router();
 const workout = require("../../models/workout");
 
-router.post("/api/workout", async ({ body }, res) => {
+const urlParams = new URL(document.location).searchParams
+var search = urlParams.get('id')
+
+router.post("/workouts", async ({ body }, res) => {
     try {
         const workoutData = await workout.create(body)
 
+        console.log(workoutData)
         res.json(workoutData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+router.get("/workouts", async ({ body }, res) => {
+    try {
+        const workoutData = await workout.find({})
+
+        console.log(workoutData)
+        res.json(workoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put(`/workouts/${search}`, async ({ body }, res) => {
+    try {
+        const workoutData = await workout.findOneAndUpdate({})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
